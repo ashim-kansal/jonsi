b2c406ad43fab04c04aed0aa5f7dec47cc88dd66
ashim kansal <kansal.136@gmail.com>
2023-04-22 21:13:08 +0530

    google integration

--- lib/screens/ProviderScreens/dashboard/widget/home_item.dart
+++ lib/screens/ProviderScreens/dashboard/widget/home_item.dart
@@ -2,58 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../../../components/AppColors.dart';
-class HomeItem extends StatelessWidget {
-  /// Creates a widget combines of [InkWell], [Container], [Expanded].
-  ///
-  /// At least one of [label], [value], [onClick] must not be null.
-  /// This widget used to set header for on boarding screens like [LoginScreen], [ForgotPasswordScreen], [LoginWithNumberScreen].
-  ///
-  /// This contains [title], [label] and [imagePath]{asset/} to show top header.
-  HomeItem({
-    Key ?key,
-    required this.title,
-    required this.imagePath,
-    Function()? onTap,
-  }): super(key: key);
 
-  final String title;
-  final String imagePath;
-  Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-        onTap: onTap,
-        child: Card(
+Widget HomeItem({
+  String? title,
+  String? imagePath,
+  Function()? onTap,
+}){
+  return InkWell(
+    onTap: onTap,
+    child: Card(
       elevation: 2,
       color: Colors.white,
       child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:  [
-              Image.asset(imagePath,
-                  height: 80,
-                  width: 80,
-                  fit: BoxFit.cover),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:  [
+            Image.asset(imagePath!,
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover),
 
-              SizedBox(
-                height: 10,
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              title!,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: AppColors.text_desc,
+                  fontSize: 12,
+                  fontFamily: "Montserrat-Medium"
               ),
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: AppColors.text_desc,
-                    fontSize: 12,
-                    fontFamily: "Montserrat-Medium"
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
+      ),
 
-        ),
-    );
-  }
+    ),
+  );
 }
